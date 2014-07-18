59e08a66e1c4261a1a82c2386b608ec420e11181
rs028 <rob.sommariva@gmail.com>
2014-07-18 16:28:43 +0100

    Add setup info

---
+++ main.R
@@ -0,0 +1,9 @@
+library(chron)
+
+source("atmosChem.R")
+source("convertUnits.R")
+source("massSpec.R")
+source("physChem.R")
+source("processData.R")
+source("referenceData.R")
+source("utilityFuncs.R")
