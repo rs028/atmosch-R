94552e9f7370b9d9487308ee4492c4014a4d906d
rs028 <rob.sommariva@gmail.com>
2017-06-09 19:09:28 +0100

    Fix operators

--- atmosPhys.R
+++ atmosPhys.R
@@ -106,38 +106,38 @@ fSolar <- function(lat, long, dt.chron) {
   ##                           SEA = solar elevation angle )
   ## ---------------------------------------------------------------------
   ## latitude and longitude in radians
-  lat.r = fConvAngle(lat,'deg','rad')
-  long.r = fConvAngle(long,'deg','rad')
+  lat.r <- fConvAngle(lat,'deg','rad')
+  long.r <- fConvAngle(long,'deg','rad')
   ## day of year (1 Jan = 1) and fractional time
   jan1 <- chron(paste("01/01/", years(dt.chron), sep=""))
   fracd <- as.numeric(dt.chron - jan1 + 1)
   doy <- floor(fracd)
   gmt <- (fracd - doy) * 24
   ## day angle
-  nday = doy - 1
-  theta = 2 * pi * (nday / 365)
+  nday <- doy - 1
+  theta <- 2 * pi * (nday / 365)
   ## sun declination
-  b0 =  0.006918
-  b1 = -0.399912
-  b2 =  0.070257
-  b3 = -0.006758
-  b4 =  0.000907
-  b5 = -0.002697
-  b6 =  0.001480
-  dec = b0 + b1 * cos(theta) + b2 * sin(theta) + b3 * cos(2 * theta) +
-        b4 * sin(2 * theta) + b5 * cos(3 * theta) + b6 * sin(3 * theta)
+  b0 <-  0.006918
+  b1 <- -0.399912
+  b2 <-  0.070257
+  b3 <- -0.006758
+  b4 <-  0.000907
+  b5 <- -0.002697
+  b6 <-  0.001480
+  dec <- b0 + b1 * cos(theta) + b2 * sin(theta) + b3 * cos(2 * theta) +
+         b4 * sin(2 * theta) + b5 * cos(3 * theta) + b6 * sin(3 * theta)
   ## local hour angle
-  c0 =  0.000075
-  c1 =  0.001868
-  c2 = -0.032077
-  c3 = -0.014615
-  c4 = -0.040849
-  eqt = c0 + c1 * cos(theta) + c2 * sin(theta) + c3 * cos(2 * theta) +
-        c4 * sin(2 * theta)
-  lha = pi * (gmt / 12 - (1 + long / 180)) + eqt
+  c0 <-  0.000075
+  c1 <-  0.001868
+  c2 <- -0.032077
+  c3 <- -0.014615
+  c4 <- -0.040849
+  eqt <- c0 + c1 * cos(theta) + c2 * sin(theta) + c3 * cos(2 * theta) +
+         c4 * sin(2 * theta)
+  lha <- pi * (gmt / 12 - (1 + long / 180)) + eqt
   ## solar zenith angle and solar elevation angle
-  sza = acos(sin(dec) * sin(lat.r) + cos(dec) * cos(lat.r) * cos(lha))
-  sea = pi/2 - sza
+  sza <- acos(sin(dec) * sin(lat.r) + cos(dec) * cos(lat.r) * cos(lha))
+  sea <- pi/2 - sza
   ## output data.frame
   df.out <- data.frame(dt.chron, dec, lha, sza, sea)
   colnames(df.out) <- c("GMT","DEC","LHA","SZA","SEA")
