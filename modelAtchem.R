3dea8a4ea4b122d3c9f5bfac29bcc6d9500004cc
rs028 <rob.sommariva@gmail.com>
2019-02-18 10:09:55 +0000

    Add fConstrGap() function

--- modelAtchem.R
+++ modelAtchem.R
@@ -1,24 +1,28 @@
 ### ---------------------------------------------------------------- ###
-### functions for the AtChem/MCM model:
-### - fAtchemIn()  : AtChem input files
+### functions for the AtChem/MCM model (https://github.com/AtChem/AtChem2):
+### - fAtchemIn()  : AtChem constraint files
 ### - fAtchemOut() : AtChem output files
+### - fConstrGap() : gaps in constraint files
 ###
-### version 1.3, Aug 2017
+### version 1.4, Jun 2018
 ### author: RS
 ### ---------------------------------------------------------------- ###
 
-fAtchemIn <- function(input.dir, input.df, start.str) {
-  ## Generate input files for the AtChem/MCM model:
+fAtchemIn <- function(constr.dir, constr.df, start.str) {
+  ## Generate constraint files for the AtChem/MCM model:
   ## * concentrations (molecule cm-3)
   ## * temperature (K)
   ## * pressure (mbar)
   ## * relative humidity (%)
-  ## * photolysis rates (s-1)
+  ## * water concentration (molecule cm-3)
   ## * sun declination (rad)
+  ## * boundary layer height (cm)
+  ## * dilution rate (s-1)
+  ## * photolysis rates (s-1)
   ## * JFAC (scaling factor for photolysis rates)
   ##
-  ## NB: the data.frame with the input data must have one datetime
-  ## chron variable (TIME) and one or more data variables:
+  ## NB: the data.frame with the constraint data must have one
+  ## datetime chron variable (TIME) and one or more data variables:
   ##
   ##        TIME           variable 1    variable 2    variable 3
   ##  21-01-15 12:00:00        10            25            30
@@ -27,34 +31,34 @@ fAtchemIn <- function(input.dir, input.df, start.str) {
   ##  ...                      ...           ...           ...
   ##
   ## input:
-  ##     input.dir = directory for input files
-  ##     input.df = input data.frame
+  ##     constr.dir = constraint files directory
+  ##     constr.df = data.frame with model constraints
   ##     start.str = model start datetime string ("d-m-y h:m:s")
   ## output:
   ##     init = list of values at model start datetime
-  ##     --> input files in `input.dir`
+  ##     --> constraint files in `constr.dir`
   ## ------------------------------------------------------------
-  if (!is.data.frame(input.df)) {
-    df.name <- deparse(substitute(input.df))
+  if (!is.data.frame(constr.df)) {
+    df.name <- deparse(substitute(constr.df))
     stop(paste(df.name, "must be a data.frame", sep=" "))
   }
   ## time in seconds since model start
   model.start <- fChronStr(start.str, "d-m-y h:m:s")
-  input.df$SEC <- round((input.df$TIME - model.start) * 86400)
+  constr.df$SEC <- round((constr.df$TIME - model.start) * 86400)
   ## write each variable to file and make list of initial values
   init <- list()
-  for (c in 2:(ncol(input.df)-1)) {
-    inp.str <- fVarName(input.df[c])
+  for (c in 2:(ncol(constr.df)-1)) {
+    inp.str <- fVarName(constr.df[c])
     ## set negative values to zero and remove rows with NaN
-    inp.var <- ifelse(input.df[[c]] < 0, 0, input.df[[c]])
-    df.in <- data.frame(SEC=input.df$SEC, VAR=inp.var)
+    inp.var <- ifelse(constr.df[[c]] < 0, 0, constr.df[[c]])
+    df.in <- data.frame(SEC=constr.df$SEC, VAR=inp.var)
     df.in <- df.in[which(!is.na(df.in$VAR)),]
     ## find initial value of variable
     a1 <- fFindIdx(df.in$SEC, "LE", 0)
     init <- rbind(init, c(inp.str, df.in[a1,2]))
-    ## write variable to file
-    in.str <- fVarName(input.df[c])
-    write.table(df.in, file=paste(input.dir, in.str, sep=""),
+    ## save constraint file
+    in.str <- fVarName(constr.df[c])
+    write.table(df.in, file=paste(constr.dir, in.str, sep=""),
                 sep="\t", row.names=F, col.names=F)
   }
   return(init)
@@ -65,7 +69,7 @@ fAtchemOut <- function(output.dir, output.lst, start.str) {
   ## * concentration of chemical species
   ## * environment variables
   ## * photolysis rates
-  ## * model parameters
+  ## * diagnostic variables
   ##
   ## input:
   ##       output.dir = model output directory
@@ -79,18 +83,77 @@ fAtchemOut <- function(output.dir, output.lst, start.str) {
     stop(paste(lst.name, "must be a list", sep=" "))
   }
   ## load output files
-  df.res <- read.delim(paste(output.dir, output.lst[1], sep=""), header=TRUE, sep="")
+  df.res <- read.table(paste(output.dir, output.lst[1], sep=""),
+                       header=TRUE, sep="")
   if (length(output.lst) > 1) {
     for (i in 2:length(output.lst)) {
-      res.i <- read.delim(paste(output.dir, output.lst[i], sep=""), header=TRUE, sep="")
+      res.i <- read.table(paste(output.dir, output.lst[i], sep=""),
+                          header=TRUE, sep="")
       df.res <- merge(df.res, res.i, by="t")
     }
   }
-  ## add timestamp (model time in seconds since start)
+  ## model time (seconds since start) and datetime (chron)
   df.out <- data.frame(SEC=df.res$t)
   df.out$datetime <- df.out$SEC/86400 + fChronStr(start.str, "d-m-y h:m:s")
   ## output data.frame
   df.out <- cbind(df.out, df.res[-1])
   colnames(df.out) <- toupper(colnames(df.out))
+  return(df.out)
+}
+
+fConstrGap <- function(constr.dir, constr.lst, max.gap, fn.str) {
+  ## Parse the AtChem constraint files and find gaps in the data that
+  ## are larger than a given time interval.
+  ##
+  ## Optional: save plots of model constraints to pdf file.
+  ##
+  ## NB: use fAtchemIn() to generate the constraint files.
+  ##
+  ## input:
+  ##     constr.dir = constraint files directory
+  ##     constr.lst = list of constraint files
+  ##     max.gap = data gap threshold (seconds)
+  ##     fn.str = name of pdf file to save plots OR ""
+  ## output:
+  ##     df.out = data.frame ( )
+  ##     --> pdf file : `fn.str`.pdf
+  ## ------------------------------------------------------------
+  if (!is.list(constr.lst)) {
+    lst.name <- deparse(substitute(constr.lst))
+    stop(paste(lst.name, "must be a list", sep=" "))
+  }
+  ## open pdf file to save plots
+  if (fn.str != "") {
+    pdf(paste(fn.str, ".pdf", sep=""), paper="a4r", width=0, height=0)
+  }
+  ## import constraint files
+  data.df <- data.frame()
+  for (constr in constr.lst) {
+    constr.df <- read.table(paste(constr.dir, constr, sep=""))
+    ## flag gaps in data
+    constr.df$V3 <- c(NA, diff(constr.df$V1, 1))
+    constr.df$V4 <- ifelse(constr.df$V3 > max.gap, 0, 1)
+    flag1 <- which(constr.df$V4 != 1)
+    flag2 <- flag1 - 1
+    ## add name of constraint variable and time interval
+    if (length(flag1) >= 1) {
+      sec.df <- data.frame(VARIABLE=constr,
+                           START=constr.df[flag2,1],
+                           STOP=constr.df[flag1,1])
+      data.df <- rbind(data.df, sec.df)
+    }
+    ## make plot [optional]
+    plot(constr.df[,1], constr.df[,2], type="b",
+         main=constr, xlab="seconds", ylab="")
+    points(constr.df[flag2,1], constr.df[flag2,2], pch=16, col="green", cex=2)
+    points(constr.df[flag1,1], constr.df[flag1,2], pch=16, col="red", cex=2)
+  }
+  ## close pdf file
+  if (fn.str != "") {
+    dev.off()
+  }
+  ## output data.frame
+  data.df$DELTA <- data.df$STOP - data.df$START
+  df.out <- data.df[order(data.df$START),]
   return(df.out)
 }
