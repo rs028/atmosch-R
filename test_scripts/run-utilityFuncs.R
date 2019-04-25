936f9ccb431bfb40babca3dab229a6845e729365
rs028 <rob.sommariva@gmail.com>
2019-04-25 19:46:57 +0100

    Add fVarName() test

--- test_scripts/run-utilityFuncs.R
+++ test_scripts/run-utilityFuncs.R
@@ -5,4 +5,17 @@
 require(testit)
 
 ## -----------------------------------------------
-## 
+## input data
+
+df1 <- data.frame(Temp = c(298,300,302),
+                  Press = c(101300,101400,101500),
+                  H2O = c(1e19,2e19,3e19))
+
+## -----------------------------------------------
+## ## check input and output
+
+assert("=> fVarName() input/output",
+       fVarName(df1$Temp) == fVarName(df1["Temp"]),
+       fVarName(df1$Temp) == fVarName(df1[1])
+       fVarName(df1[c("Temp","Press")]) == fVarName(df1[1:2])
+       )
