386c26fb1e5c522eb8e59889cbf5f6760275dc00
rs028 <rob.sommariva@gmail.com>
2023-07-24 11:04:35 +0100

    Source main does not change wd

--- main.R
+++ main.R
@@ -1,28 +1,26 @@
 ### ---------------------------------------------------------------- ###
 ### atmosch-R                                                        ###
 ### ---------------------------------------------------------------- ###
-### Load the atmosch-R functions into the R workspace.
+### Load the atmosch-R functions and the required packages into
+### the R workspace.
 ###
-### version 2.6, Apr 2021
+### version 2.7, Jul 2023
 ### author: RS
 ### ---------------------------------------------------------------- ###
 library(chron)
 
-## go to the repository directory
-setwd(f.repo)
+source(paste0(f.repo, "atmosChem.R"))
+source(paste0(f.repo, "atmosPhys.R"))
+source(paste0(f.repo, "convertUnits.R"))
+source(paste0(f.repo, "fileUtils.R"))
+source(paste0(f.repo, "instrumentFiles.R"))
+source(paste0(f.repo, "massSpec.R"))
+source(paste0(f.repo, "modelAtchem.R"))
+source(paste0(f.repo, "physChem.R"))
+source(paste0(f.repo, "processData.R"))
+source(paste0(f.repo, "referenceData.R"))
+source(paste0(f.repo, "utilityFuncs.R"))
 
-## source the atmosch-R files
-source("atmosChem.R")
-source("atmosPhys.R")
-source("convertUnits.R")
-source("fileUtils.R")
-source("instrumentFiles.R")
-source("massSpec.R")
-source("modelAtchem.R")
-#source("obsoleteFuncs.R")   # obsolete functions (uncomment if needed)
-source("physChem.R")
-source("processData.R")
-source("referenceData.R")
-source("utilityFuncs.R")
-
-### ---------------------------------------------------------------- ###
+## ------------------------------------------ ##
+## obsolete functions (uncomment if needed)
+#source(paste0(f.repo, "obsoleteFuncs.R"))
