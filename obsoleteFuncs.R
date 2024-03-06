413ed497fa5661c79a822dd707e425329f7f0d7d
rs028 <rob.sommariva@gmail.com>
2024-03-06 10:47:31 +0000

    Move fConvSI to obsoleteFuncs.R

--- obsoleteFuncs.R
+++ obsoleteFuncs.R
@@ -2,11 +2,141 @@
 ### atmosch-R                                                        ###
 ### ---------------------------------------------------------------- ###
 ### obsolete functions:     [ KEEP FOR BACKWARD COMPATIBILITY ]
+### - fConvSI()
 ### - fFindPnt()     ==>  use fFindIdx()
 ### - fRead_TS42i()  ==>  use fRead_Thermo()
 ###
 ### author: RS
 ### ---------------------------------------------------------------- ###
+
+fConvSI <- function(data.in, unit.in, unit.out) {
+  ## Convert between multiples and fractions of SI units:
+  ## * peta      = "P"
+  ## * tera      = "T"
+  ## * giga      = "G"
+  ## * mega      = "M"
+  ## * kilo      = "K"
+  ## * hecto     = "H"
+  ## * deca      = "D"
+  ## * base unit = "-"
+  ## * deci      = "d"
+  ## * centi     = "c"
+  ## * milli     = "m"
+  ## * micro     = "u"
+  ## * nano      = "n"
+  ## * pico      = "p"
+  ## * femto     = "f"
+  ##
+  ## INPUT:
+  ##     data.in = data in original unit
+  ##     unit.in = original measurement unit
+  ##     unit.out = final measurement unit
+  ## OUTPUT:
+  ##     data.out = data in final unit
+  ## EXAMPLE:
+  ##     xx <- fConvSI(4321, "u", "-")
+  ## ------------------------------------------------------------
+  ## data in original unit to reference unit (base unit)
+  switch(unit.in,
+         "P" = {
+           data.ref <- data.in * 1.0e+15
+         },
+         "T" = {
+           data.ref <- data.in * 1.0e+12
+         },
+         "G" = {
+           data.ref <- data.in * 1.0e+09
+         },
+         "M" = {
+           data.ref <- data.in * 1.0e+06
+         },
+         "K" = {
+           data.ref <- data.in * 1.0e+03
+         },
+         "H" = {
+           data.ref <- data.in * 1.0e+02
+         },
+         "D" = {
+           data.ref <- data.in * 1.0e+01
+         },
+         "-" = {
+           data.ref <- data.in
+         },
+         "d" = {
+           data.ref <- data.in * 1.0e-01
+         },
+         "c" = {
+           data.ref <- data.in * 1.0e-02
+         },
+         "m" = {
+           data.ref <- data.in * 1.0e-03
+         },
+         "u" = {
+           data.ref <- data.in * 1.0e-06
+         },
+         "n" = {
+           data.ref <- data.in * 1.0e-09
+         },
+         "p" = {
+           data.ref <- data.in * 1.0e-12
+         },
+         "f" = {
+           data.ref <- data.in * 1.0e-15
+         },
+         stop("INPUT ERROR: unit not found")
+         )
+  ## data in reference unit (base unit) to final unit
+  switch(unit.out,
+         "P" = {
+           data.out <- data.ref / 1.0e+15
+         },
+         "T" = {
+           data.out <- data.ref / 1.0e+12
+         },
+         "G" = {
+           data.out <- data.ref / 1.0e+09
+         },
+         "M" = {
+           data.out <- data.ref / 1.0e+06
+         },
+         "K" = {
+           data.out <- data.ref / 1.0e+03
+         },
+         "H" = {
+           data.out <- data.ref / 1.0e+02
+         },
+         "D" = {
+           data.out <- data.ref / 1.0e+01
+         },
+         "-" = {
+           data.out <- data.ref
+         },
+         "d" = {
+           data.out <- data.ref / 1.0e-01
+         },
+         "c" = {
+           data.out <- data.ref / 1.0e-02
+         },
+         "m" = {
+           data.out <- data.ref / 1.0e-03
+         },
+         "u" = {
+           data.out <- data.ref / 1.0e-06
+         },
+         "n" = {
+           data.out <- data.ref / 1.0e-09
+         },
+         "p" = {
+           data.out <- data.ref / 1.0e-12
+         },
+         "f" = {
+           data.out <- data.ref / 1.0e-15
+         },
+         stop("INPUT ERROR: unit not found")
+         )
+  ## data in final unit
+  return(data.out)
+}
 
 fFindPnt <- function(vecd, ops, xval, xst) {
   ## find the first point greater/equal or less/equal than a reference
@@ -91,7 +221,7 @@ fRead_TS49i <- function(data.dir, data.fn, data.log, data.var=NULL) {
   if (data.log == "iport") {       # log with iPort
     data.df <- read.table(data.file, header=TRUE, sep="", skip=5)
     data.df$Time <- paste(data.df$Time, "00", sep=":")
-  } else if (data.log == "term"){  # log with terminal
+  } else if (data.log == "term") {  # log with terminal
     data.df <- read.table(data.file, header=F, sep="")
     if (!is.null(data.var)) {  # user-set streaming variables
       print(ncol(data.df))
